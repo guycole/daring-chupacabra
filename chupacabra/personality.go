58065f0328bd1a5786cc0a4dc337a4ba3af453bf
Guy Cole <guycole@gmail.com>
2022-07-30 01:16:02 -0700

    working unit test

---
+++ chupacabra/personality.go
@@ -0,0 +1,62 @@
+// Copyright 2022 Guy Cole. All rights reserved.
+// Use of this source code is governed by a GPL-3 license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"errors"
+	"log"
+)
+
+type userType struct {
+	name string
+}
+
+const maxUsers = 10
+
+type userArrayType [maxUsers]*userType
+
+var userArray userArrayType
+
+func newUser(name string) (*userType, error) {
+	if len(name) < 1 {
+		return nil, errors.New("bad name")
+	}
+
+	result := userType{name: name}
+	return &result, nil
+}
+
+func userAdd(name string) (int, error) {
+	for ndx := 0; ndx < maxUsers; ndx++ {
+		if (userArray[ndx] != nil) && (userArray[ndx].name == name) {
+			return -1, errors.New("duplicate name")
+		}
+	}
+
+	candidate, err := newUser(name)
+	if err != nil {
+		log.Println(err)
+		return -1, err
+	}
+
+	for ndx := 0; ndx < maxUsers; ndx++ {
+		if userArray[ndx] == nil {
+			userArray[ndx] = candidate
+			return ndx, nil
+		}
+	}
+
+	return -1, errors.New("userArray full")
+}
+
+func userDelete(name string) int {
+	for ndx := 0; ndx < maxUsers; ndx++ {
+		if userArray[ndx] == nil {
+			userArray[ndx] = nil
+			return ndx
+		}
+	}
+
+	return -1
+}
---
+++ chupacabra/personality_test.go
@@ -0,0 +1,30 @@
+// Copyright 2022 Guy Cole. All rights reserved.
+// Use of this source code is governed by a GPL-3 license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+/*
+func TestNewUser(t *testing.T) {
+	name := "Testaroo"
+
+}
+*/
+
+func TestNewUserBadName(t *testing.T) {
+	user, err := newUser("")
+	if err == nil {
+		t.Fatalf("empty user should generate error")
+	}
+	log.Println(user)
+
+	/*
+		    if user != "" || err == nil {
+		        t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+			}
+	*/
+}
