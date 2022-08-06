638524bbffdf4b539b41a06cc6942d3bd9a2a383
Guy Cole <guycole@gmail.com>
2022-08-06 00:15:51 -0700

    working minikube deployment and service to nginx

---
+++ infra/start_minikube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Title:start_minikube.sh
+# Description: start minikube
+# Development Environment: OS X 10.13.6
+# Author: G.S. Cole (guycole at gmail dot com)
+#
+minikube start --memory=6g --nodes=1 --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0 --vm=true --kubernetes-version=v1.24.3
+minikube addons enable ingress
+#
