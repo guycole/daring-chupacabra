d17841c9161bc032b3eff88b9fd51396e52f7b7f
Guy Cole <guycole@gmail.com>
2022-08-12 10:14:05 -0700

    add prometheus

--- chupacabra/back_end.go
+++ chupacabra/back_end.go
@@ -5,29 +5,27 @@ package main
 
 import (
 	"context"
-	"time"
+	"encoding/json"
 
 	"log"
 
 	redis "github.com/go-redis/redis/v8"
 )
 
-type RequestType struct {
-	RequestId    string
-	RequestType  string
+const maxPayloadArguments = 5
+
+type argumentArrayType [maxPayloadArguments]string
+
+type PayloadType struct {
+	ArgumentSize int
+	Arguments    argumentArrayType
+	PayloadId    string
+	PayloadType  string
 	ReplyChannel string
 }
 
-type ResponseType struct {
-	RequestId   string
-	RequestType string
-	Response    string
-}
-
-//rdb *redis.Client
-
 func backEnd() {
-	log.Println("back end")
+	log.Println("backEnd entry")
 
 	// TODO get these arguments from secrets
 	rdb := redis.NewClient(&redis.Options{
@@ -36,58 +34,29 @@ func backEnd() {
 		DB:       0, // use default DB
 	})
 
-	channelName := "admin_in"
+	channelName := "back_end"
 
-	log.Println("prepare to Subscribe")
 	topic := rdb.Subscribe(context.Background(), channelName)
-	log.Println("back from Subscribe")
-	log.Println(topic)
 
 	for {
 		// blocking read
 		message, err := topic.ReceiveMessage(context.Background())
 		if err != nil {
 			log.Println(err)
-			log.Println("requestFromWorker skipping bad receive message")
+			log.Println("backEnd skipping bad receive message")
 			continue
 		}
 
 		log.Println(message)
-	}
 
-	/*
-		var rt RequestType
-		err = json.Unmarshal([]byte(msg.Payload), &rt)
-		if err != nil {
-				log.Println(err)
-				log.Println("requestFromManager skipping bad unmarshal")
-				continue
-		}
-
-		requestQueue.enqueue(&rt)
-	*/
-	//}
-
-	/*
-		rt := RequestType{Name: "name", RequestId: "reqid", ArgumentSize: 11}
-
-		payload, err := json.Marshal(rt)
+		var pt PayloadType
+		err = json.Unmarshal([]byte(message.Payload), &pt)
 		if err != nil {
 			log.Println(err)
+			log.Println("backEnd skipping bad unmarshal")
+			continue
 		}
 
-		log.Println(payload)
-
-		err = rdb.Publish(context.Background(), channel, payload).Err()
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-
-	for true {
-		log.Println("Infinite Loop 2")
-		time.Sleep(time.Second * 10)
+		log.Println(pt)
 	}
 }
-
-//
